32c1646fcd5fa67bd8b1dda980772c01f83f6792
MARK-JALTERNATE\MJ65 <mj65@live.nl>
2019-03-13 20:40:07 +0100

    first commit tutorial-esque setup

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,3 @@
+module.exports = (client, member) => {
+    member.send(`Welcome on the server! Please be aware that we won't tolerate troll, spam or harassment. Have fun 😀`)
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const fs = require('fs')
+
+fs.readdir('./events/', (err, files) => {
+    files.forEach(file => {
+        const eventHandler = require(`./events/${file}`)
+        const eventName = file.split('.')[0]
+        client.on(eventName, arg => eventHandler(client, arg))
+    })
+})
+
+client.login(process.env.BOT_TOKEN)
+
+require('http').createServer().listen()
