81e045d05e0e98bdc2a88f95929beff5ae3de380
MARK-JALTERNATE\MJ65 <mj65@live.nl>
2019-03-30 01:25:46 +0100

    comments/documentation index.js

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ const guildSql = require('./sqlite/guildSql');
 const client = new Discord.Client();
 client.sql = new guildSql(); // Singleton of GuildSql inside the Client object
 
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir('./events/', (err, files) => { // eventHandler
-    files.forEach(file => {
-        const eventHandler = require(`./events/${file}`)
-        const eventName = file.split('.')[0]
-        client.on(eventName, arg => eventHandler(client, arg))
-    })
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split('.')[0]
+    client.on(eventName, arg => eventHandler(client, arg))
+  })
 })
 
 client.commands = new Enmap();
 
+// This loop reads the /commands/ folder and attaches each command to the client.commands Enmap.
 fs.readdir("./commands/", (err, files) => { // commandHandler
   if (err) return console.error(err);
-  files.forEach(file => { 
+  files.forEach(file => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
     let commandName = file.split(".")[0];
