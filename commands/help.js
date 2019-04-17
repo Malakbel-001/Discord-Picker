40fcd5b293bfd8eb968cf9aa0588e760dab2e4cd
MARK-JALTERNATE\MJ65 <mj65@live.nl>
2019-04-17 23:19:38 +0200

    default cooldown to 1 second

--- commands/help.js
+++ commands/help.js
@@ -41,7 +41,7 @@ module.exports = {
 		if (command.description) data.push(`**Description:** ${command.description}`);
 		if (command.usage) data.push(`**Usage:** ${message.client.sql.getPrefix(message.guild)}${command.name} ${command.usage}`);
 
-		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+		data.push(`**Cooldown:** ${command.cooldown || 1} second(s)`);
 
 		message.channel.send(data, { split: true });
 	},
--- events/message.js
+++ events/message.js
@@ -32,7 +32,7 @@ module.exports = (client, message) => {
 
 	const now = Date.now();
 	const timestamps = client.cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
+	const cooldownAmount = (command.cooldown || 1) * 1000;
 
 	if (timestamps.has(message.author.id)) {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
