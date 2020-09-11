e19cd95a7749ac91deb9d05a69a76f8471c81878
MARK-JALTERNATE\MJ65 <mj65@live.nl>
2020-09-11 16:57:47 +0200

    pseudo code todo

--- commands/setupReact.js
+++ commands/setupReact.js
@@ -7,5 +7,14 @@ module.exports = {
 		message.channel.send("hi")
 			.then(newMessage => console.log(`Sent message: ${newMessage.id}`))
 			.catch(console.error);
+
+		// Pseudo code
+		// Save the channel id to Guild
+		// Save the message id to Guild
+		// Add reaction to new message
+
+		// Inside messageReactionAdd
+		// Update ChannelId -> MessageId -> Message who reacted to the post
+		// aka Update post with who reacted
 	},
 };
